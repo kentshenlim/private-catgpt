ea363f405cbc787bf6c403600100a13881deac0e
kentshenlim <kentshen_lim24@hotmail.com>
2025-03-09 21:58:35 +0800

    Scroll down on entering

--- README.md
+++ README.md
@@ -10,16 +10,14 @@ Chat app with OpenAI integration. Using gpt-4o-mini.
 - [ ] Dynamic design, light theme dark theme
 - [ ] Deploy to Vercel
 - [ ] Dark and light theme toggle
-- [ ] Limit length of prompt
 - [ ] Remove public procedure of open ai endpoints
 - [ ] Maybe limit token sliding window
 - [ ] Store in SQL
 - [ ] Remove example components
 - [ ] Error catching
-- [ ] Scroll down on entering
 - [ ] When no message
 - [ ] Need to move is thinking state to context so that conversation can render
       conditionally
 - [ ] localStorage if no SQL
-- [ ] Disable empty prompt
 - [ ] Change font if needed
+- [ ] Form validation (min length, max length, is empty, both client and server side)
--- src/app/_components/Conversation.tsx
+++ src/app/_components/Conversation.tsx
@@ -3,9 +3,16 @@
 import { useConversation } from "./ConversationProvider";
 import Markdown from "react-markdown";
 import { AnimatedCat } from "@/lib/ui";
+import { useRef, useEffect } from "react";
 
 export default function Conversation() {
   const { conversation, isSystemThinking } = useConversation();
+  const scrollerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (conversation[conversation.length - 1]?.role === "user")
+      scrollerRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [conversation]);
 
   return (
     <div className="flex flex-col gap-y-8 px-6 pb-12 pt-4 text-base/7 [&_li]:my-2 [&_ol]:my-2 [&_ul]:my-2">
@@ -21,6 +28,7 @@ export default function Conversation() {
           <AnimatedCat />
         </article>
       )}
+      <div ref={scrollerRef}></div>
     </div>
   );
 }
