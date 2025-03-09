4cd841f6597030ddffcb4a9b09c9e703db7529c7
kentshenlim <kentshen_lim24@hotmail.com>
2025-03-09 21:47:28 +0800

    Submit form on enter

--- src/app/_components/Prompt.tsx
+++ src/app/_components/Prompt.tsx
@@ -2,14 +2,15 @@
 
 import { SendHorizontal } from "lucide-react";
 import { useConversation } from "./ConversationProvider";
-import { type FormEvent } from "react";
-import { useState } from "react";
+import { KeyboardEvent, type FormEvent } from "react";
+import { useState, useRef } from "react";
 import { api } from "@/trpc/react";
 
 export default function Prompt() {
   const { appendMessage, conversation, isSystemThinking, setIsSystemThinking } =
     useConversation();
   const [userMessage, setUserMessage] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
   const sendMessage = api.openAI.chatCompletionPrompt.useMutation(); // client - tRPC
 
   function handleSubmit(e: FormEvent) {
@@ -31,10 +32,22 @@ export default function Prompt() {
     );
   }
 
+  function handleEnterPress(e: KeyboardEvent) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      const form = formRef.current;
+      if (form)
+        form.dispatchEvent(
+          new Event("submit", { bubbles: true, cancelable: true }),
+        );
+    }
+  }
+
   return (
     <form
       className="flex flex-col rounded-2xl bg-primary py-2 [&>textarea]:bg-primary"
       onSubmit={handleSubmit}
+      ref={formRef}
     >
       <textarea
         name="prompt"
@@ -46,11 +59,12 @@ export default function Prompt() {
           setUserMessage(e.target.value);
         }}
         value={userMessage}
+        onKeyDown={handleEnterPress}
       />
       <button
         type="submit"
         className="self-end rounded-full bg-accent p-2 text-sm text-background hover:bg-accent/70 disabled:cursor-not-allowed disabled:opacity-40"
-        disabled={isSystemThinking}
+        disabled={isSystemThinking || userMessage.length === 0}
       >
         <SendHorizontal strokeWidth={2} size={20} />
       </button>
