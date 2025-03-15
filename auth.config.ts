fc78314d74898cef645ceef2d29a5384b6731640
kentshenlim <kentshen_lim24@hotmail.com>
2025-03-15 17:03:47 +0800

    Redirect to login page if not authenticated

---
+++ auth.config.ts
@@ -0,0 +1,19 @@
+import { type NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isAtProtected = nextUrl.pathname === "/"; // Home = chat app is protected
+      console.log(isAtProtected);
+      if (isAtProtected)
+        return isLoggedIn; // At protected route must log in
+      else if (isLoggedIn) return Response.redirect(new URL("/")); // Not at protected path but log in, redirect to chat page
+      return true; // No need authentication for unprotected routes
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
---
+++ auth.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "auth.config";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const { ADMIN_Email: correctEmail, ADMIN_PASSWORD: correctPassword } =
+            process.env;
+          if (!correctEmail || !correctPassword) return null;
+          if (email !== correctEmail) return null;
+          const passwordMatch = await bcrypt.compare(password, correctPassword);
+          if (passwordMatch) return { email };
+        }
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
