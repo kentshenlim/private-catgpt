92846b167e75d3937664f47e89dfc531997296e0
kentshenlim <kentshen_lim24@hotmail.com>
2025-03-16 20:15:43 +0800

    Replace with fish bone

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,7 @@
 import CatPawNormal from "@/lib/ui/CatPawNormal";
 import CatPawSharp from "@/lib/ui/CatPawSharp";
-import CatPawRound from "@/lib/ui/CatPawRound";
-import CatPawLines from "@/lib/ui/CatPawLines";
+import CatTail from "@/lib/ui/CatTail";
+import FishBone from "@/lib/ui/FishBone";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const size = 200;
@@ -11,17 +11,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="absolute top-0 hidden rotate-45 sm:block md:scale-150 lg:scale-[200%]">
         <CatPawNormal size={size} />
       </div>
-      <div className="absolute bottom-0 right-0 hidden -rotate-45 sm:block">
-        <CatPawSharp size={size} />
-      </div>
       <div className="absolute bottom-0 right-0 hidden -rotate-45 sm:block lg:scale-125">
         <CatPawSharp size={size} />
       </div>
-      <div className="absolute bottom-[5%] left-[10%] hidden rotate-[15deg] lg:block lg:scale-150">
-        <CatPawRound size={size} />
+      <div className="absolute bottom-0 left-[10%] hidden rotate-[15deg] lg:block">
+        <CatTail />
       </div>
-      <div className="absolute bottom-1/2 right-[10%] hidden -rotate-[37deg] scale-150 xl:block">
-        <CatPawLines size={size} />
+      <div className="absolute bottom-1/2 right-[10%] hidden rotate-[37deg] xl:block">
+        <FishBone size={200} />
       </div>
     </>
   );
---
+++ src/lib/ui/CatTail.tsx
@@ -0,0 +1,13 @@
+export default function CatTail() {
+  return (
+    <svg
+      width="200"
+      height="400"
+      viewBox="0 0 200 400"
+      xmlns="http://www.w3.org/2000/svg"
+      className="fill-accent"
+    >
+      <path d="M120 400c-20-20-40-50-50-90s-10-100 10-140c20-40 40-50 50-30 10 20-10 50-20 90s-10 90 10 130c20 40 20 50 0 50z" />
+    </svg>
+  );
+}
